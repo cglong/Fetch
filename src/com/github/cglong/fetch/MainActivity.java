1f7c35b0e94db137c254315ce37a6577353d0d92
Chris Long <chrislong@gatech.edu>
2013-01-15 20:08:18 -0500

    Send message to server and display its response

--- src/com/github/cglong/fetch/MainActivity.java
+++ src/com/github/cglong/fetch/MainActivity.java
@@ -1,6 +1,12 @@
 package com.github.cglong.fetch;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -22,14 +28,33 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	/**
-	 * 
-	 */
 	public void sendMessage(View view) {
 		EditText editMessage = (EditText) findViewById(R.id.edit_message);
 		CharSequence message = editMessage.getText();
+		new RetrieveResponseTask().execute(message);
+	}
+	
+	private class RetrieveResponseTask extends AsyncTask<CharSequence, Void, String> {
+		protected String doInBackground(CharSequence... messages) {
+			String uri = getString(R.string.string_uri);
+			
+			try {
+				Socket socket = new Socket(uri, 8080);
+				PrintWriter outgoing = new PrintWriter(socket.getOutputStream(), true);
+				outgoing.println(messages[0]);
+
+				BufferedReader incoming = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String response = incoming.readLine();
+				socket.close();
+				return response;
+			} catch (Exception e) {
+				return e.getMessage();
+			}
+		}
 		
-		TextView textResponse = (TextView) findViewById(R.id.text_response);
-		textResponse.setText(message);
+		protected void onPostExecute(String response) {
+			TextView textResponse = (TextView) findViewById(R.id.text_response);
+			textResponse.setText(response);
+		}
 	}
 }
